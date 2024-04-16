08dabe847f7f9c2edc8a0b0b8b1db37180c5b839
decobraz <docenkoefim08@gmail.com>
2024-04-16 22:51:28 +0300

    add basic html

--- tetrisonline/play/views.py
+++ tetrisonline/play/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h4>Пошел нахуй</h4>")
-
-
-def about(request):
-    return HttpResponse("<h4>Пошел нахуй в кубе</h4>")
+    return render(request, 'play/index.html')
