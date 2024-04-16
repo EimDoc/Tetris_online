6fdbdaea57ed34efab3bb428c5926a68741950df
decobraz <docenkoefim08@gmail.com>
2024-04-16 22:16:39 +0300

    add some urls

--- tetrisonline/play/views.py
+++ tetrisonline/play/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<h4>Пошел нахуй</h4>")
+
+
+def about(request):
+    return HttpResponse("<h4>Пошел нахуй в кубе</h4>")
